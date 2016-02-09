1867e8ffa2ca3d87cd7ff606125e039dbaeb38e3
Garrison Taylor <garrisontaylor@gmail.com>
2016-02-09 16:02:22 -0500

    Refactor code

--- camera.cpp
+++ camera.cpp
@@ -12,11 +12,45 @@
 using namespace cv;
 using namespace std;
 
-int main(int argc, char* argv[])
-{
-  VideoCapture capture = VideoCapture(0);  
-  if(!capture.isOpened())
-    return -1;
+Mat getBWImage(VideoCapture cap, Mat intrinsic, Mat distCoeffs) {
+  //Dilation
+  const int dilationSize = 2;
+  const Mat dilateElement = getStructuringElement(MORPH_RECT, Size(2*dilationSize + 1, 2*dilationSize + 1), Point(dilationSize, dilationSize));
+
+  //Blur
+  const int kernelSize = 8*1+ 1;
+
+  //HLS Thresholding
+  const int H_low = 73;
+  const int H_high = 116;
+  const int S_low = 197;
+  const int S_high = 255;
+  const int L_low = 154;
+  const int L_high = 255;
+  const Scalar low = Scalar(H_low, L_low, S_low);
+  const Scalar high = Scalar(H_high, L_high, S_high);
+
+  Mat img;
+  Mat imgFixed;
+  Mat blurredImg;
+  Mat imgThresh;
+  Mat dilatedImg;
+  Mat hls;
+  cap >> img;
+  //Undistortion processing
+  undistort(img, imgFixed, intrinsic, distCoeffs);
+  imshow( "image", imgFixed );
+  //Blur
+  GaussianBlur(imgFixed,blurredImg,Size(kernelSize,kernelSize), 1);
+  //HLS Threshold processing
+  cvtColor(blurredImg, hls, COLOR_BGR2HLS);
+  inRange(hls, low, high, imgThresh);
+  //Dilation
+  dilate(imgThresh, dilatedImg,dilateElement);
+  return dilatedImg;
+}
+
+Mat getIntrinsic() {
   //Undistortion constants
   Mat intrinsic = Mat(3,3,CV_32FC1);
   intrinsic.ptr<float>(0)[0] = 567.3694188707971;
@@ -28,26 +62,25 @@ int main(int argc, char* argv[])
   intrinsic.ptr<float>(2)[0] = 0;
   intrinsic.ptr<float>(2)[1] = 0;
   intrinsic.ptr<float>(2)[2] = 1;
+  return intrinsic;
+}
+
+Mat getDistCoeffs() {
   Mat distCoeffs = Mat(1,5,CV_32FC1);
   distCoeffs.ptr<float>(0)[0] = -0.463731090351821;
   distCoeffs.ptr<float>(0)[1] = 0.5114874359918231;
   distCoeffs.ptr<float>(0)[2] = -0.003444501644466447;
   distCoeffs.ptr<float>(0)[3] = 0.0005056629007096351;
   distCoeffs.ptr<float>(0)[4] = -0.6914960232636986;
-  Mat imgFixed;
+  return distCoeffs;
+}
 
-  //HLS Threshold
-  //HLS = Hue Luminance Saturation
-  const int H_low = 73;
-  const int H_high = 116;
-  const int S_low = 197;
-  const int S_high = 255;
-  const int L_low = 154;
-  const int L_high = 255;
-  Mat hls;
-  const Scalar low = Scalar(H_low, L_low, S_low);
-  const Scalar high = Scalar(H_high, L_high, S_high);
-  Mat imgThresh;
+int main(int argc, char* argv[])
+{
+  VideoCapture capture = VideoCapture(0);  
+  if(!capture.isOpened())
+    return -1;
+  
   //Contours
   const int minArea = 500;
   int minPerimeter;
@@ -63,38 +96,20 @@ int main(int argc, char* argv[])
   vector<Vec4i> hierarchy;
   const Scalar color = Scalar(255,255,255);
 
-  //Dilation
-  const int dilationSize = 2;
-  Mat dilatedImg;
-  const Mat dilateElement = getStructuringElement(MORPH_RECT, Size(2*dilationSize + 1, 2*dilationSize + 1), Point(dilationSize, dilationSize));
-
-  //Blur
-  Mat blurredImg;
-  const int kernelSize = 8*1+ 1;
-
   //Angle and distance detection
   const double pi = 3.14159265;
   const int z = 89;
   const int h_naught = 12;
-  const double f = 100000;
+  const double f = 10000;
   const int w_naught = 20;
   
   //capture.set(CV_CAP_PROP_FRAME_WIDTH, 1920);
   //capture.set(CV_CAP_PROP_FRAME_HEIGHT, 1080);
-  Mat img;
+
   while(1)
   {
-    capture >> img;
-    //Undistortion processing
-    undistort(img, imgFixed, intrinsic, distCoeffs);    
-    imshow( "image", imgFixed );
-    //Blur
-    GaussianBlur(imgFixed,blurredImg,Size(kernelSize,kernelSize), 1); 
-    //HLS Threshold processing
-    cvtColor(blurredImg, hls, COLOR_BGR2HLS);
-    inRange(hls, low, high, imgThresh);
-    //Dilation
-    dilate(imgThresh, dilatedImg,dilateElement);
+    Mat dilatedImg;
+    dilatedImg = getBWImage(capture, getIntrinsic(), getDistCoeffs());
     //Contours processing
     Canny(dilatedImg, canny_output, thresh, thresh*2, 3 );
     findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0,0) );
@@ -122,7 +137,7 @@ int main(int argc, char* argv[])
     }
     imshow("Contours", drawing);
 
-        //Determine which contour to use.
+    //Determine which contour to use.
     //We'll assume there's only one contour for now, but this needs to be fixed.
     for(int i = 0; i < goodContours.size(); i++)
     {
@@ -144,6 +159,5 @@ int main(int argc, char* argv[])
     waitKey(1);
   }
   capture.release();
-  img.release();  
   return 0;
 }
