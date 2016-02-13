9fcb44f81c9ced419d999a76fb060b8aaec25a27
Garrison Taylor <garrisontaylor@gmail.com>
2016-02-13 12:37:22 -0500

    Add back some important things

--- compile.sh
+++ compile.sh
@@ -2,7 +2,9 @@
 rm -rf build
 mkdir build
 echo "Compiling Main Camera Code"
-g++ -ggdb `pkg-config opencv --cflags --libs` camera.cpp -o build/camera `pkg-config --libs opencv`
+g++ -ggdb `pkg-config opencv --cflags --libs` camera.cpp -o build/camera `pkg-config --libs opencv` -Wall
 echo "Compiling Camera Calibration Code"
 g++ -ggdb `pkg-config opencv --cflags --libs` camera_calibration.cpp -o build/camera_calib `pkg-config --libs opencv`
-./build/camera
+if [ $1 = "run" ] ; then
+  ./build/camera
+fi
