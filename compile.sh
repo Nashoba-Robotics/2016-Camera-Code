55a44e2e484114b601ab5a0b06e5ef5e6c6fc579
Garrison Taylor <garrisontaylor@gmail.com>
2016-02-13 12:25:57 -0500

    Revert to before ioctle

--- compile.sh
+++ compile.sh
@@ -2,11 +2,7 @@
 rm -rf build
 mkdir build
 echo "Compiling Main Camera Code"
-g++ -L /usr/local/cuda-6.5/lib -ggdb `pkg-config opencv --cflags` camera.cpp -o build/camera `pkg-config --libs opencv` -Wall
+g++ -ggdb `pkg-config opencv --cflags --libs` camera.cpp -o build/camera `pkg-config --libs opencv`
 echo "Compiling Camera Calibration Code"
-g++ -L /usr/local/cuda-6.5/lib -ggdb `pkg-config opencv --cflags` camera_calibration.cpp -o build/camera_calib `pkg-config --libs opencv` -Wall
-echo "Compiling Network Testing Code"
-gcc network.c -o build/network -Wall
-if [ $1 = "run" ] ; then
-  ./build/camera
-fi
+g++ -ggdb `pkg-config opencv --cflags --libs` camera_calibration.cpp -o build/camera_calib `pkg-config --libs opencv`
+./build/camera
